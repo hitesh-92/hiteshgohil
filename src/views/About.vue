31bbedd80c14c62da4d78de80e58afffb84deb8a
hitesh-92 <hiteshgohil@hotmail.co.uk>
2019-07-31 03:57:32 +0100

    about description edited

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,32 +1,45 @@
 <template>
   <section class="px-4 mx-1 py-3">
 
-    <h1 class="font-weight-light pb-3 display-1">About</h1>
+    <h1 class="font-weight-light pb-3 display-1">A little about me</h1>
 
     <!--  https://drive.google.com/open?id=1DZR10zrGSurABQ9C1C8tkugLYuklKzQcLL7zVvT4X6s -->
 
-    <!-- <section id="description" class="">
-      <p>I am a self taugt developer who started to learn and write code about 2 years ago</p>
-      <p>I initially started by picking up a book: Python for Bash and Shell scripting</p>
-      <p>(if you don't know what that is, don't worry I didn't either)</p>
-      <p>I started in the wrong place but found my way to web development and Linux</p>
-      <p>Since, I have developed a few projects and made websites for local businesses. Most of the work involved front-end development</p>
-      <p>Currently I build applications using various frameworks and libraries related to JavaScript</p>
-    </section> -->
-
-    <section id="description" class="">
-      <p>I am a self taught developer who started to learn about coding around 2 years ago and I didn’t start in a conventional way</p>
-      <p>With little prior knowledge of compSci or anything related to the tech space, I made the decision to buy a book: Python for Bash and Shell scripting</p>
+    <section id="description">
+      <p>Self taught Developer. Who started in the wrong place...</p>
+      <p>Walking through Waterstones (yes, I still read)</p>
+      <p>A book caught my eye</p>
+      <p><strong>Python for Bash and Shell scripting</strong></p>
       <p><em>Who knows what that is, right?</em></p>
-      <p>Reading around the topics of the book discovered Linux</p>
-      <p>Throwing yourself into the world of Linux is like jumping into an abyss. <span><em>Just like staring into the terminal for the first time</em></span></p>
-      <p>However, unlike an abyss, my time spent learning Python came to an end. I hadn’t even written my first Hello World app. I didn’t even know the basics.</p>
-      <p>A few months later discovered Web Development.</p>
-      <p><em>Another can of worms, ready to be opened</em></p>
-      <p>Picking the basics is easy enough, given a little time. JavaScript allows you to see what real programming is capable of and is what intrigued me to pursue this further</p>
+      <p>I picked it up, flicked through it and bought it</p>
+      <p>What's the worst that can happen?</p>
+      <p>I went through the book. I didn't understand much</p>
+      <p>Shell, Bash, environments... None of it made sense</p>
+      <p>I was heading up the creek without a paddle</p>
+      <p>So I started reading around the topics and discovered Linux</p>
+      <p>Let me tell you this. Throwing yourself into the world of Linux is like jumping into an abyss</p>
+      <p><em>Like the first time you open a terminal!</em></p>
+      <p>After a few attempts going over the same parts</p>
+      <p>Again and again. And again</p>
+      <p>The frustration grew</p>
+      <p><strong>I was making no progress</strong></p>
+      <p>Trail and error. It didn't work for me here</p>
+      <p>It was the end of the line</p>
+      <p>I guess you can say I saw the bottom of an abyss</p>
+      <p><em>Hopelessness.</em></p>
+      <p>I hadn’t written my first Hello World app</p>
+      <p>I didn’t know the basics (or what they were)</p>
+      <p><em>I even gave away the book.</em></p>
+      <p>A few months passed...</p>
+      <p>Still persistent. Learning (as always)</p>
+      <p><strong>I discovered Web Development - online</strong></p>
+      <p>Who knew so much (free) info was out there!?!</p>
+      <p><em>Another can of worms, waiting to be opened</em></p>
+      <p>Picking the basics is easy enough, given a little time</p>
+      <p>JavaScript allows you to see what real programming is capable of</p>
+      <p>It's what intrigued me to pursue web development further</p>
+      <p><em>I hope that book helps someone as much as it helped me. <strong>Silver Lining</strong></em></p>
     </section>
-
-
 
     <v-divider class="ma-5"></v-divider>
 
@@ -61,6 +74,7 @@
 
   </section>
 </template>
+
 
 <script>
 export default {
