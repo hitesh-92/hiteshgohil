9cba5c48927cd4fc48a1b65850c92ad77b687b5c
hitesh-92 <hiteshgohil@hotmail.co.uk>
2019-07-31 19:23:01 +0100

    updated contact page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,46 +1,16 @@
 <template>
   <section class="px-4 mx-1 py-3">
-    <h1 class="font-weight-light pb-3 display-1">Contact</h1>
+    <h1 class="font-weight-light pb-3 display-1">Get In Touch</h1>
 
-    <div class="pb-4">
+    <div class="my-1">
       <p>Get in touch through your email client <a class="transaprent" href="mailto:hiteshgohil@gmx.co.uk"> <i class="fas fa-envelope pl-2 mail"></i>  </a></p>
-      <p>Or drop me a message using the contact form</p>
+      <p>Or send me a message me at:</p>
     </div>
 
-    <div v-if="responseError" class="red--text ma-3">
-        <p>Oops! Something went wrong! Help me improve your experience by reporting this error</p>
-        <p>You can message me <a href="mailto:hiteshgohil@hotmail.co.uk">here</a>, look forward to hearing from you!</p>
+    <div class="my-1">
+      <p>Hiteshgohil@gmx.co.uk</p>
     </div>
 
-    <v-form ref="form" v-model="valid" id="form" class="elevation-2 grey lighten-4 mb-4">
-      <v-container class="pa-4">
-          <v-layout wrap>
-
-            <v-flex xs12 md6>
-                <v-text-field prepend-icon="perm_identity" v-model="firstname" :rules="nameRules" :counter="10" label="First name" required></v-text-field>
-            </v-flex>
-
-            <v-flex xs12 md6>
-                <v-text-field v-model="lastname" :rules="nameRules" :counter="10" label="Last name" required></v-text-field>
-            </v-flex>
-
-            <v-flex xs12>
-                <v-text-field prepend-inner-icon="alternate_email" v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
-            </v-flex>
-
-            <v-flex x12>
-                <v-textarea prepend-inner-icon="message" v-model="message" :rules="messageRules" box label="Your Message">
-                </v-textarea>
-            </v-flex>
-
-            <v-flex xs12 offset-md5 offset-xs7>
-                <v-btn @click="validate" :disabled="!valid" class="green white--text">submit</v-btn>
-            </v-flex>
-
-          </v-layout>
-      </v-container>
-    </v-form>
-        
   </section>    
 </template>
 
